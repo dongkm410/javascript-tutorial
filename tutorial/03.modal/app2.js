ccb47c28484193f70ee9887c977362e5d8d50bac
dkm <dongkyungmin@gmail.com>
2023-02-19 10:19:58 +0900

    sticky

--- tutorial/03.modal/app2.js
+++ tutorial/03.modal/app2.js
@@ -1,12 +1,10 @@
-const btns = document.querySelectorAll('.btn');
 const content = document.querySelector('.content');
 const modal = document.querySelector('.modal');
+const btns = document.querySelectorAll('.btn');
 
 btns.forEach(function (btn) {
   btn.addEventListener('click', function () {
-    content.classList.toggle('.active');
-    modal.classList.toggle('.active');
+    content.classList.toggle('active');
+    modal.classList.toggle('active');
   });
 });
-
-console.log(btns);
